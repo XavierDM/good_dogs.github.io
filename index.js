d96df673c3c5d2ee711fac79e635995d14d346cd
XavierDM <xavier.de.moor@gmail.com>
2021-09-08 09:53:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-/* let btn = document.getElementById('searchBtn') */
-//let image = document.getElementById('image')
-/* let breed = inputVal.value */
-/* let knop = document.getElementById('subtmit'); */
-//var insertion = document.querySelector("inputVal");
-/* let breed = document.querySelector("inputVal") */
-/* var breed = insertion.value */
-/* var breed = document.querySelector('inputVal').value */
-/* console.log(insertion) */
-
-
-/* btn.addEventListener('click', () =>{
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res=> res.json())
-    .then(result => {
-        console.log(result)
-        image.src = result.message
-    })
-    .catch(err=>console.log(err))
-    console.log(insertion)
-}) */
-
-/* knop.addEventlistener('click', (event)=>{
-fetch(`https://dog.ceo/api/${breed}/hound/images`)
-    .then(response => response.json())
-    .then((data) => {
-        img.src = event.image
-        console.log(data);
-    })
-    .catch(err=>console.log(err))
-}); */
-/* const form = document.querySelector("#form")
-form.addEventListener('submit', (e) => {
-    
-    var input = document.querySelector("#inputVal")
-    document.write(input.value)
-    console.log(input.value)
-    e.preventDefault
-}) */
-
-
-
-
-
-
 let container = document.querySelector('container')
 console.log(container)
 let btn = document.getElementById('searchBtn')
@@ -51,14 +6,7 @@ btn.addEventListener('click', () => {
     var insertionV = insertion.value
     var insertionVL = insertionV.toLowerCase()
     console.log(insertion.value)
-    /*     fetch('https://dog.ceo/api/breeds/hound/list')
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data);
-            })
-            .catch(function (response) {
-                console.log("error!")
-            }); */
+
     if (insertionVL == "") {
         alert("You forgot to enter a dog breed, here's a random dog image.")
         fetch("https://dog.ceo/api/breeds/image/random")
@@ -88,32 +36,17 @@ btn.addEventListener('click', () => {
             .catch(err => console.log(err))
     } else {
         fetch(`https://dog.ceo/api/breed/${insertionVL} / images / random / `)
-        .then(res => res.json())
-        .then(result => {
-            if (result.status === "error") {
-                alert(`Sorry, ${insertionVL}  isn't a dog breed`)
-            } else {
-                console.log(result)
-                alert(`Aren't ${insertionVL} the best?`)
-                image.src = result.message
-            }
+            .then(res => res.json())
+            .then(result => {
+                if (result.status === "error") {
+                    alert(`Sorry, ${insertionVL}  isn't a dog breed`)
+                } else {
+                    console.log(result)
+                    alert(`Aren't ${insertionVL} the best?`)
+                    image.src = result.message
+                }
 
-        })
+            })
 
     }
 })
-
-
-/* form.addEventListener('submit', () => {
-    var insertion = document.querySelector("#inputVal")
-    var insertionV = insertion.value
-    var insertionVL = insertionV.toLowerCase()
-    console.log(insertion.value)
-    fetch(`https://dog.ceo/api/breed/${insertionVL}/images/random`)
-        .then(res => res.json())
-        .then(result => {
-            console.log(result)
-            image.src = result.message
-        })
-        .catch(err => console.log(err))
-) */
